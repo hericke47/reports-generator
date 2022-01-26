5c38b22e99a5d0953c5d9a00852fd69737f91bc7
herick.exterkoetter <herickherick47@gmail.com>
2022-01-26 16:21:21 -0300

    switch case elixir

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,5 +1,12 @@
 defmodule ReportsGenerator do
-  def hello do
-    :world
+  def build(filename) do
+    case File.read("reports/#{filename}") do
+      {:ok, result} ->
+        result
+
+      {:error, result} ->
+        result
+        # _ -> "caso qualquer" default do case
+    end
   end
 end
