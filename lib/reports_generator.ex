4707e6e9cf5f3f8b9b10cfd29cf42f58cab2e937
herick.exterkoetter <herickherick47@gmail.com>
2022-01-28 09:52:27 -0300

    split characters whith order, id, name and price

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -2,6 +2,13 @@ defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
     |> File.stream!()
-    |> Enum.each(fn elem -> IO.inspect(elem) end)
+    |> Enum.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
   end
 end
